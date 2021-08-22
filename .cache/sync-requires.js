fc987b49589df042f6a6d276a1248d499fe37bfc
MoinShaikhSquare <87135904+MoinShaikhSquare@users.noreply.github.com>
2021-08-22 14:16:44 +0530

    gatsby build

---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\admin\\Desktop\\gatsby-speed\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\admin\\Desktop\\gatsby-speed\\src\\pages\\404.js")),
+  "component---src-pages-index-jsx": preferDefault(require("C:\\Users\\admin\\Desktop\\gatsby-speed\\src\\pages\\index.jsx")),
+  "component---src-pages-page-2-js": preferDefault(require("C:\\Users\\admin\\Desktop\\gatsby-speed\\src\\pages\\page-2.js")),
+  "component---src-pages-rough-jsx": preferDefault(require("C:\\Users\\admin\\Desktop\\gatsby-speed\\src\\pages\\rough.jsx"))
+}
+
