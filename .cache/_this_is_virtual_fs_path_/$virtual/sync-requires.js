17fa842d792d4dc34dce2e8c156598f07959e358
MoinShaikh611 <87135904+MoinShaikhSquare@users.noreply.github.com>
2021-08-22 16:18:52 +0530

    added public and build files

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\admin\\Desktop\\gatsby-speed\\.cache\\dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("C:\\Users\\admin\\Desktop\\gatsby-speed\\src\\pages\\404.js")),
   "component---src-pages-index-jsx": preferDefault(require("C:\\Users\\admin\\Desktop\\gatsby-speed\\src\\pages\\index.jsx")),
   "component---src-pages-page-2-js": preferDefault(require("C:\\Users\\admin\\Desktop\\gatsby-speed\\src\\pages\\page-2.js")),
---
+++ public/component---src-pages-rough-jsx-f984634f961678bf18f1.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[324],{8913:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var a=n(7294),r=n(5444);function u(){return a.createElement(a.Fragment,null,a.createElement("h2",null,a.createElement(r.Link,{to:"/"}," back to home")),a.createElement("h1",null,"Rough"))}}}]);
+//# sourceMappingURL=component---src-pages-rough-jsx-f984634f961678bf18f1.js.map
